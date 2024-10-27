81856016a1a75c6f874dd0b5c3eb52a2fdc30e44
kasraalitabar <kasraalitabar@gmail.com>
2024-10-27 02:12:46 -0700

    consol

---
+++ main.js
@@ -0,0 +1,10 @@
+var num1 =Number(prompt("enter your mark"));
+var num2 =Number(prompt("enter your mark"));
+var num3 =Number(prompt("enter your mark"));
+var num4 =Number(prompt("enter your mark"));
+var num5 =Number(prompt("enter your mark"));
+
+var result = (num1 + num2 + num3 + num4 + num5) / 5 
+
+
+console.log(result)
